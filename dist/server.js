26e120febffb78426e54f57eb89d492897fd05b2
Luka <simjanovic.luka@gmail.com>
2017-10-28 12:41:06 +0200

    release

--- dist/server.js
+++ dist/server.js
@@ -1045,7 +1045,7 @@ exports.default = async function server() {
         from: `${ctx.request.body.test.name} <${ctx.request.body.test.mail}>`,
         to: 'balr.stuff@gmail.com',
         subject: ctx.request.body.test.sub,
-        text: `MEJL JE POSLAO ${ctx.request.body.mail} Tekst mejla ${ctx.request.body.test.mess}`
+        text: `MEJL JE POSLAO ${ctx.request.body.test.mail} Tekst mejla ${ctx.request.body.test.mess}`
       };
 
       transporter.sendMail(mailerOptions, function (err, res) {
--- kit/entry/server.js
+++ kit/entry/server.js
@@ -190,7 +190,7 @@ export default (async function server() {
           from: `${ctx.request.body.test.name} <${ctx.request.body.test.mail}>`,
           to: 'balr.stuff@gmail.com',
           subject: ctx.request.body.test.sub,
-          text: `MEJL JE POSLAO ${ctx.request.body.mail} Tekst mejla ${ctx.request.body.test.mess}`,
+          text: `MEJL JE POSLAO ${ctx.request.body.test.mail} Tekst mejla ${ctx.request.body.test.mess}`,
       }
       
       transporter.sendMail(mailerOptions, function (err, res) {
